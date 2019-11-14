ca41449e17f5351dddeeab9e3ef6e974797da0d4
Guillaume HARARI <guillaumeharari@hotmail.com>
2019-11-14 23:49:07 +0100

    test

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * Date: 11/10/17
  * Time: 14:01
  */
-
+var_dump('index');die;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/debug.php';
 require_once __DIR__ . '/../config/db.php';
