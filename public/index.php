160f74a7bdcbb7ddcafedf3873469941b53c8584
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2019-11-15 10:57:31 +0100

    test

--- public/index.php
+++ public/index.php
@@ -5,12 +5,11 @@
  * Date: 11/10/17
  * Time: 14:01
  */
+
+if (getenv('ENV') === false || getenv('ENV') != 'prod') {
+    require_once __DIR__ . '/../config/debug.php';
+    require_once __DIR__ . '/../config/db.php';
+}
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
-
-if (getenv('ENV') != 'prod') {
-   // require_once __DIR__ . '/../config/debug.php';
-   // require_once __DIR__ . '/../config/db.php';
-}
-
