6f9e10362baa3cc2049f53ae13d7f4da62d30979
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2019-11-15 10:43:07 +0100

    test

--- src/Model/Connection.php
+++ src/Model/Connection.php
@@ -63,9 +63,6 @@ class Connection
                 $this->user,
                 $this->password
             );
-            var_dump('mysql:host=' . $this->host . '; dbname=' . $this->dbName . '; charset=utf8',
-                $this->user,
-                $this->password);die;
             $this->pdoConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             // show errors in DEV environment
