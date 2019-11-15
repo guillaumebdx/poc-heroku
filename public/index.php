db12fd6da64f96eed7a624116978e40257b0a221
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2019-11-15 10:04:02 +0100

    test

--- public/index.php
+++ public/index.php
@@ -5,13 +5,12 @@
  * Date: 11/10/17
  * Time: 14:01
  */
-var_dump(getenv('ENV'));die;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
 
 if (getenv('ENV') != 'prod') {
-    require_once __DIR__ . '/../config/debug.php';
-    require_once __DIR__ . '/../config/db.php';
+   // require_once __DIR__ . '/../config/debug.php';
+   // require_once __DIR__ . '/../config/db.php';
 }
 
